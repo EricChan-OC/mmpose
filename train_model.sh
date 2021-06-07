bddfafbbc84b4479112afccb367bea879a09010c
Ubuntu <ubuntu@ip-172-31-8-27.ec2.internal>
2021-06-07 17:47:42 +0000

    add configs and infer horse video

--- train_model.sh
+++ train_model.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-python3 tools/train.py myConfigs/horse/train_leg_front_resnet.py --work-dir temp_logs/horse_leg_front/resnet
+#python3 tools/train.py myConfigs/horse/train_leg_front_resnet.py --work-dir temp_logs/horse_leg_front/resnet
 
-python3 tools/train.py myConfigs/horse/train_leg_back_resnet.py --work-dir temp_logs/horse_leg_back/resnet
+#python3 tools/train.py myConfigs/horse/train_leg_back_resnet.py --work-dir temp_logs/horse_leg_back/resnet
 
-python3 tools/train.py myConfigs/horse/train_head_hrnet.py --work-dir temp_logs/horse_head/hrnet
+#python3 tools/train.py myConfigs/horse/train_head_hrnet.py --work-dir temp_logs/horse_head/hrnet
 
-python3 tools/train.py myConfigs/horse/train_tail_hrnet.py --work-dir temp_logs/horse_tail/hrnet
+#python3 tools/train.py myConfigs/horse/train_tail_hrnet.py --work-dir temp_logs/horse_tail/hrnet
 
-python3 tools/train.py myConfigs/horse/train_leg_front_hrnet.py --work-dir temp_logs/horse_leg_front/hrnet
+#python3 tools/train.py myConfigs/horse/train_leg_front_hrnet.py --work-dir temp_logs/horse_leg_front/hrnet
 
-python3 tools/train.py myConfigs/horse/train_leg_back_hrnet.py --work-dir temp_logs/horse_leg_back/hrnet
+#python3 tools/train.py myConfigs/horse/train_leg_back_hrnet.py --work-dir temp_logs/horse_leg_back/hrnet
+python3 tools/train.py myConfigs/horse/train_head_right_resnet.py --work-dir temp_logs/horse_head_right/resnet
+
+python3 tools/train.py myConfigs/horse/train_head_left_resnet.py --work-dir temp_logs/horse_head_left/resnet
+
