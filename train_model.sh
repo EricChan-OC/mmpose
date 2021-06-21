5614ef164b250901dbb7e224e2015f16a12e21c9
Ubuntu <ubuntu@ip-172-31-1-8.ec2.internal>
2021-06-21 19:53:40 +0000

    update configs

--- train_model.sh
+++ train_model.sh
@@ -1,33 +1,15 @@
 #!/bin/sh
 
-# python3 tools/train.py myConfigs/horse/train_spine_resnet_black.py --work-dir training_result/horse_spine_black/resnet
-# python3 tools/train.py myConfigs/horse/train_spine_resnet_green.py --work-dir training_result/horse_spine_green/resnet
-# python3 tools/train.py myConfigs/horse/train_spine_resnet_grey.py --work-dir training_result/horse_spine_grey/resnet
-# python3 tools/train.py myConfigs/horse/train_spine_resnet_white.py --work-dir training_result/horse_spine_white/resnet
-
-# python3 tools/train.py myConfigs/horse/train_leg_front_resnet_black.py --work-dir training_result/horse_leg_front_black/resnet
-# python3 tools/train.py myConfigs/horse/train_leg_front_resnet_green.py --work-dir training_result/horse_leg_front_green/resnet
-# python3 tools/train.py myConfigs/horse/train_leg_front_resnet_grey.py --work-dir training_result/horse_leg_front_grey/resnet
-# python3 tools/train.py myConfigs/horse/train_leg_front_resnet_white.py --work-dir training_result/horse_leg_front_white/resnet
-
-# python3 tools/train.py myConfigs/horse/train_leg_back_resnet_black.py --work-dir training_result/horse_leg_back_black/resnet
-# python3 tools/train.py myConfigs/horse/train_leg_back_resnet_green.py --work-dir training_result/horse_leg_back_green/resnet
-# python3 tools/train.py myConfigs/horse/train_leg_back_resnet_grey.py --work-dir training_result/horse_leg_back_grey/resnet
-
-#python3 tools/train.py myConfigs/horse/train_leg_back_resnet_white.py --work-dir training_result/horse_leg_back_white/resnet
-#python3 tools/train.py my_configs/cattle/resnet_50/head_black.py --work-dir training_result/cattle/resnet_50/head_black/
-
-
 for animal in 'cattle'
 do
-  for model in 'resnet_50' 'resnet_150'
+  for model in 'resnet_50'
   do
-    for color in 'black' 'green' 'grey' 'white'
+    for color in 'grey' 'black'
     do
-      for part in 'head' 'tail' 'spine' 'leg_front' 'leg_back'
+      for part in 'head' 'spine' 'tail' 'leg_front' 'leg_back'
       do
-        echo my_configs/$animal/$model/$part'_'$color.py train_result/$animal/$model/$part_$color >> train_model.txt
-        python3 tools/train.py my_configs/$animal/$model/$part'_'$color.py --work-dir train_result/$animal/$model/$part_$color
+        echo my_configs/$animal/$model'_10'/$part'_'$color.py train_result/$animal/$model'_10'/$part'_'$color/ >> train_model.txt
+        python3 tools/train.py my_configs/$animal/$model'_10'/$part'_'$color.py --work-dir train_result/$animal/$model'_10'/$part'_'$color/
       done
     done
   done
